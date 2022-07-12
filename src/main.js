dc967b88f2bfd3591244ac18083f9acabf69dd0e
zaynlannister <zaynlannister@gmail.com>
2022-07-12 22:18:36 +0500

    add video actions

--- src/main.js
+++ src/main.js
@@ -60,10 +60,10 @@ class Video {
 
     setMuteOrUnmute(classList, item) {
         if (item.muted) {
-            replacerClass(classList, 'fa-volume-mute', 'fa-volume-up')
+            replacerClass(classList, 'fa-volume-xmark', 'fa-volume-high')
             item.muted = false;
         } else {
-            replacerClass(classList, 'fa-volume-up', 'fa-volume-mute')
+            replacerClass(classList, 'fa-volume-high', 'fa-volume-xmark')
             item.muted = true;
         }
     }
@@ -85,7 +85,7 @@ class Video {
                         <video id="${this.id}" width="400px" height="500px" muted autoplay
                                src="${this.src}"></video>
                         <i data-id="${this.id}" class="fas fa-pause video-data__show-play__pause"></i>
-                        <i data-id="${this.id}" class="fas fa-volume-mute video-data__show-mute"></i>
+                        <i data-id="${this.id}" class="fas fa-volume-xmark video-data__show-mute"></i>
                     </div>
                     <div class="video-data__actions">
                         <div data-id="${this.id}" class="video-data__actions-block video-data__actions-likes">
@@ -110,10 +110,80 @@ class Video {
     }
 }
 
+// Business Logic
+
+function checkClassList(className, currentVideo, item) {
+    if (className[2] === 'video-data__show-play__pause') {
+        item.setPauseOrPlay(className, currentVideo)
+    } else if (className[2] === 'video-data__show-mute') {
+        item.setMuteOrUnmute(className, currentVideo)
+    }
+}
+
+function findClickedVideo(target, dataId, videoList, videoPlayer) {
+    if (target) {
+        videoList.forEach(item => {
+            if (item.id === parseInt(dataId)) {
+                let needVideo = Array.from(videoPlayer).find(item => {
+                    return item.id === dataId
+                });
+                checkClassList(target.classList, needVideo, item)
+            }
+        })
+    }
+}
+
+function clickSubscribeButton(button, buttonParent) {
+    button.addEventListener('click', () => {
+        if (buttonParent.classList.contains('active')) {
+            button.innerHTML = 'Подписаться'
+            buttonParent.classList.remove('active')
+        } else {
+            button.innerHTML = 'Ты подписан'
+            buttonParent.classList.add('active')
+        }
+    })
+}
+
+function openCommentsButton(item) {
+    let commentMenu = document.querySelector('.comments');
+
+    commentMenu.classList.toggle('active')
+}
+
+function renderComment(value, container) {
+    container.innerHTML += `
+            <div class="comments-content">
+                <div class="comments-content__data">
+                    <div class="comments-content__img"></div>
+                    <div class="comments-content__title">
+                        <div class="comments-content__title-name">*Name*</div>
+                        <div class="comments-content__title-text">${value}</div>
+                    </div>
+                </div>
+                <div class="comments-content__likes">
+                    <i class="comments-content__likes-button far fa-heart"></i>
+                    <div class="comments-content__likes-amount">0</div>
+                </div>
+            </div>
+        `
+}
+
+function addLikeButton(element, checkText) {
+    if (typeof element === 'object' && checkText === 'videoButton') {
+        element.addEventListener('click', el => {
+            let amountOfLikes = element.childNodes[4]
+            amountOfLikes.innerHTML++
+        })
+    } else if (checkText === 'commentButton') {
+        element.innerHTML++
+    }
+}
+
 let videoData = [];
 
 function videoPlaylist(element, items) {
-    const DOMelement = document.querySelector(element);
+    const DOMElement = document.querySelector(element);
 
     items.forEach(item => {
         videoData.push(new Video(item));
@@ -121,11 +191,62 @@ function videoPlaylist(element, items) {
 
     function renderVideos() {
         videoData.forEach(item => {
-            DOMelement.innerHTML += item.getTemplate();
+            DOMElement.innerHTML += item.getTemplate();
         })
     }
 
     renderVideos()
+
+    let videoPlayer = document.querySelectorAll('.video-data__show video');
+    let subscribeButtons = document.querySelectorAll('.video-data__author-button button');
+    let likeButton = document.querySelectorAll('.video-data__actions-likes');
+    let commentButtons = document.querySelectorAll('.video-data__actions-comments');
+
+    // listeners
+
+    DOMElement.addEventListener('click', el => {
+        let target = el.target.closest('svg');
+        let dataId = (target) ? target.getAttribute('data-id'): false;
+
+        findClickedVideo(target, dataId, videoData, videoPlayer)
+    })
+
+    videoPlayer.forEach(video => {
+        video.addEventListener('ended', () => {
+            video.play()
+        })
+    })
+
+    subscribeButtons.forEach(button => {
+        let buttonParent = button.parentNode;
+
+        clickSubscribeButton(button, buttonParent)
+    })
+
+    commentButtons.forEach(item => {
+        item.addEventListener('click', () => {
+            openCommentsButton(item)
+        })
+    })
+
+    let sendTextButton  = document.querySelector('.comments-actions__button');
+    let commentsInput  = document.querySelector('.comments-actions__input input');
+    let commentContainer = document.querySelector('.comments-container');
+
+    sendTextButton.addEventListener('click', () => {
+        let inputValue = commentsInput.value;
+        renderComment(inputValue, commentContainer)
+        commentsInput.value = ''
+    })
+
+    commentContainer.addEventListener('click', el => {
+        let target = el.target.tagName;
+        let amountOfLikes = el.target.parentNode.querySelector('.comments-content__likes-amount')
+
+        if (target === 'svg') {
+            addLikeButton(amountOfLikes, 'commentButton')
+        }
+    })
 }
 
 videoPlaylist(".video", responseFromServer)
